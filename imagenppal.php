fc6a25e693da79ce09c63d971b5418d2d0622dc0
pablovasquez321 <70274944+pablovasquez321@users.noreply.github.com>
2020-09-02 16:49:29 -0500

    Add files via upload

---
+++ imagenppal.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>la champions league</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	<img src="fondo.jpg" class="f1">
+
+	<div class="f2">
+		<h4><p>ORIGENES DE LA CHAMPIONS</p></h4>
+	</div>
+
+	<div class="f3">
+		<h1><p>UEFA CHAMPIONS LEAGUE</p></h1>
+	</div>
+
+	<div class="f4">
+		<h4><P>EN QUE CONSISTE LA CHAMPIONS LEAGUE</P></h4>
+	</div>
+
+	<div class="f5">
+		<h4><p>MAXIMOS CAMPEONES DE LA CHAMPIONS?</p></h4>
+	</div>
+
+    <div class="f6">
+		<h4><p>FOTOS DEL TORNEO</p></h4>
+	</div>
+
+
+	<div class="C2">
+	
+	</div>
+
+	<div class="C4">
+		
+	</div>
+
+	<div class="C5">
+		
+	</div>
+
+    <div class="C6">
+    	
+    </div>
+		
+</body>
+</html>
